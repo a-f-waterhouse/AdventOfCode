ffed30efd5b22bebf1bd9fae5f06d00010f182d2
lee <lee@ECHO>
2024-12-01 08:19:16 +0000

    day 1 :)

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace day1
 {
@@ -10,6 +12,44 @@ namespace day1
     {
         static void Main(string[] args)
         {
+            int total = 0;
+
+            List<int> first = new List<int>();
+            List<int> second = new List<int>();
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(' ');
+                    first.Add(int.Parse(line[0]));
+                    second.Add(int.Parse(line[3]));
+                }
+            }
+
+            while(first.Count > 0)
+            {
+                total += (Math.Abs(first.Min() - second.Min()));
+
+                first.Remove(first.Min());
+                second.Remove(second.Min());
+            }
+            Console.WriteLine(total);
+            total = 0;
+
+            foreach (int i in first)
+            {
+                int a = 0;
+                while (second.Contains(i))
+                {
+                    a++;
+                    second.Remove(i);
+                }
+                total += i * a;
+            }
+
+            Console.WriteLine(total);
+            Console.ReadKey();
         }
     }
 }
