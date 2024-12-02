e0a04125815063b2ddf2c31efb9c41bfb6dc8fb4
lee <lee@ECHO>
2024-12-02 21:51:23 +0000

    day2

---
+++ day2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace day2
+{
+    internal class Program
+    {
+
+        static bool safe(int[] line)
+        {
+            bool d = false;
+            if (line[0] - line[1] > 0)
+            {
+                d = true;
+            }
+            for (int i = 0; i < line.Length-1; i++)
+            {
+                if (Math.Abs(line[i] - line[i+1]) > 3|| Math.Abs(line[i] - line[i + 1]) < 1)
+                {
+                    return false;
+                }
+                else if (d && line[i] - line[i + 1] < 0)
+                {
+                    return false;
+                }
+                else if (!d && line[i] - line[i + 1] > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int total = 0;
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    int[] line = sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                    if(safe(line))
+                    {
+                        total++;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            int[] newline = new int[line.Length-1];
+                            for (int j = 0; j < line.Length; j++)
+                            {
+                                if(j< i)
+                                {
+                                    newline[j] = line[j];
+                                }
+                                else if(j>i)
+                                {
+                                    newline[j-1] = line[j];
+                                }
+                            }
+                            if(safe(newline))
+                            {
+                                total++;
+                                break;
+                            }
+                            
+                        }
+                    }
+
+                }
+            }
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+
+        }
+    }
+}
