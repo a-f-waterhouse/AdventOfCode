aa35701d8b5cf90329499af504235d1c2e2a23df
lee <lee@ECHO>
2024-12-08 05:47:07 +0000

    days 6,7,8 :D

--- day7/Program.cs
+++ day7/Program.cs
@@ -4,21 +4,122 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace day7
 {
     internal class Program
     {
+
+        static bool test(long target, long[] nums)
+        {
+            long t = 0;
+            /*foreach (long num in nums)
+            {
+                t += num;
+            }
+            if (t > target)
+            {
+                return false;
+            }
+            if (t == target)
+            {
+                return true;
+            }
+            t = 1;
+            foreach (long num in nums)
+            {
+                t *= num;
+            }
+            if (t < target)
+            {
+                return false;
+            }
+            if(t ==  target)
+            {
+                return true;
+            }           */ 
+            
+            for(int i = 0; i < Math.Pow(3,nums.Length-1); i++) //1 2 3 4
+            {
+                
+                //string binary = Convert.ToString(i, 3);
+                string binary = ConvertToBase3(i);
+                while(binary.Length < nums.Length-1)
+                {
+                    binary = "0" + binary;
+                }
+                t = nums[0];
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (binary[j-1] == '1')
+                    {
+                        t *= nums[j];
+                    }
+                    else if (binary[j - 1] == '2')
+                    {
+                        t = long.Parse(t.ToString() + (nums[j].ToString()));
+                    }
+                    else
+                    {
+                        t += nums[j];
+                    }
+                }
+                //Console.WriteLine(binary + " " + t);
+                if (t == target)
+                {
+                    //Console.WriteLine();
+                    return true;
+                }
+
+            }
+           // Console.WriteLine();
+            return false;
+        }
+
+        static string ConvertToBase3(long input)
+        {
+            string result = "";
+            while (input > 0)
+            {
+                result = (input % 3).ToString() + result;
+                input /= 3;
+            }
+            
+            return result;
+            
+        }
+
         static void Main(string[] args)
         {
-            int total = 0;
-            Grid g = new Grid(File.ReadAllLines("input.txt"));
+            long total = 0;
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(':');
+                    long target = long.Parse(line[0]);
+                    long[] nums = line[1].Substring(1).Split(' ').Select(long.Parse).ToArray();
+
+                    if(test(target, nums))
+                    {
+                        total += target;
+                        Console.WriteLine(total);
+                    }
+                    else
+                    {
+                        Console.WriteLine(line[0] + ":" + line[1]);
+                    }
+
+
+                }
+            }
 
             Console.WriteLine(total);
+            Console.ReadKey();
+
             total = 0;
-
-
-
 
             Console.WriteLine(total);
         }
---
+++ day8/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace day8
+{
+    internal class Program
+    {
+        static bool ListContains(List<Coords> l, Coords c)
+        {
+            foreach(Coords cc in l)
+            {
+                if(cc.x == c.x && cc.y == c.y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void PartA()
+        {
+            Grid map = new Grid(File.ReadAllLines("input.txt"));
+            List<char> done = new List<char>();
+
+            List<Coords> antinodes = new List<Coords>();
+
+            for (int i = 0; i < map.size; i++)
+            {
+                for (int j = 0; j < map.size; j++)
+                {
+                    if (map.grid[j, i] != '.' && !done.Contains(map.grid[j, i]))
+                    {
+                        List<Coords> coords = new List<Coords>();
+                        coords = map.CoordsOf(map.grid[j, i], true);
+                        for (int k = 0; k < coords.Count; k++)
+                        {
+                            for (int l = k + 1; l < coords.Count; l++)
+                            {
+                                Coords dist = new Coords();
+                                dist.x = coords[l].x - coords[k].x;
+                                dist.y = coords[l].y - coords[k].y;
+
+                                Coords antinode = new Coords();
+                                antinode.x = coords[k].x - dist.x;
+                                antinode.y = coords[k].y - dist.y;
+
+                                if (!ListContains(antinodes, antinode) && map.withinBounds(antinode))
+                                {
+                                    antinodes.Add(antinode);
+                                }
+
+                                antinode.x = coords[l].x + dist.x;
+                                antinode.y = coords[l].y + dist.y;
+
+                                if (!ListContains(antinodes, antinode) && map.withinBounds(antinode))
+                                {
+                                    antinodes.Add(antinode);
+                                }
+                            }
+                        }
+
+                        done.Add(map.grid[j, i]);
+                    }
+                }
+            }
+        }
+
+        static void PartB()
+        {
+            Grid map = new Grid(File.ReadAllLines("input.txt"));
+            List<char> done = new List<char>();
+
+            List<Coords> antinodes = new List<Coords>();
+
+            for (int i = 0; i < map.size; i++)
+            {
+                for (int j = 0; j < map.size; j++)
+                {
+                    Coords ant = new Coords();
+                    ant.x = j;
+                    ant.y = i;
+                    if (map.grid[j,i] != '.' && !ListContains(antinodes,ant))
+                    {
+                        antinodes.Add(ant);
+                    }
+
+                    if (map.grid[j, i] != '.' && !done.Contains(map.grid[j, i]))
+                    {
+                        List<Coords> coords = new List<Coords>();
+                        coords = map.CoordsOf(map.grid[j, i], true);
+                        for (int k = 0; k < coords.Count; k++)
+                        {
+                            for (int l = k + 1; l < coords.Count; l++)
+                            {
+                                Coords dist = new Coords();
+                                dist.x = coords[l].x - coords[k].x;
+                                dist.y = coords[l].y - coords[k].y;
+
+                                Coords antinode = new Coords();
+                                int count = 1;
+                                
+                                while(map.withinBounds(antinode))
+                                {
+                                    antinode.x = coords[k].x - count* dist.x;
+                                    antinode.y = coords[k].y - count* dist.y;
+                                    if (!ListContains(antinodes, antinode) && map.withinBounds(antinode))
+                                    {
+                                        antinodes.Add(antinode);
+                                    }
+                                    count++;
+                                }
+                                antinode.x = 0;
+                                antinode.y = 0;
+                                count = 1;
+                                while (map.withinBounds(antinode))
+                                {
+                                    antinode.x = coords[l].x + count * dist.x;
+                                    antinode.y = coords[l].y + count * dist.y;
+                                    if (!ListContains(antinodes, antinode) && map.withinBounds(antinode))
+                                    {
+                                        antinodes.Add(antinode);
+                                    }
+                                    count++;
+                                }
+                            }
+                        }
+
+                        done.Add(map.grid[j, i]);
+                    }
+                }
+            }
+            map.Display(antinodes);
+            Console.WriteLine(antinodes.Count);
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            PartB();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
